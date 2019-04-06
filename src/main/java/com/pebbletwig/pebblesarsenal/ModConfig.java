8a2a0c49ab41c6d698710ab52ee7c1b6b54bd336
PebbleTwig <ejrockwood1021@gmail.com>
2019-04-06 19:15:42 -0400

    0.1.1.4

--- src/main/java/com/pebbletwig/pebblesarsenal/ModConfig.java
+++ src/main/java/com/pebbletwig/pebblesarsenal/ModConfig.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Config.LangKey("pebblesarsenal.config.title")
 public class ModConfig {
 
-        @Config.Comment("THIS CAUSES JEI DISPLAY ERRORS, PLEASE ENABLE BEFORE STARTING A NEW WORLD. Set this to true to disable anything related to anything Thermal may provide ex. Copper & Tin. Note, this will disable this mod's materials and base swords, leaving knives and greatswords. Lastly, it will prevent the items from being registered, so they will disappear if you enable this after creating the swords.")
+        @Config.Comment("Set this to true to disable anything related to anything Thermal may provide ex. Copper & Tin. Note, this will disable this mod's materials and base swords, leaving knives and greatswords. Lastly, it will prevent the items from being registered, so they will disappear if you enable this after creating any swords.")
         @Config.RequiresMcRestart
         public static boolean disableThermal = false;
 
